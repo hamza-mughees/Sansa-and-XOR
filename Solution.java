3794d11df4630f926a460c3e5e030a223b890ce1
Hamza Mughees <hamzamughees@gmail.com>
2020-06-09 01:07:00 +0100

    Added the code

---
+++ Solution.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+  public static void main(String[] args) {
+    System.out.println(sansaXor(new int[]{98,74,12}));
+  }
+
+  static int sansaXor(int[] arr) {
+    int n = arr.length;
+    if (n%2 == 0) return 0;
+    int xor = 0;
+    for (int i=0; i<n; i+=2)
+      xor ^= arr[i];
+    return xor;
+  }
+}
